28865b0d8cc41a99184ac9017d3da9246aed2f4a
Higor Chaves <hhigorsilvachaves@gmail.com>
2024-04-16 18:41:53 -0300

    first commit :)

---
+++ lib/camera_man.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'camera_man/version'
+
+module CameraMan
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/camera_man_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CameraMan do
+  it 'has a version number' do
+    expect(CameraMan::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
