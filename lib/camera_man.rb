8a2cd2bfd362f83e978c653d5b85852f60fc422a
Higor Chaves <hhigorsilvachaves@gmail.com>
2024-04-24 22:08:39 -0300

    add requires

--- lib/camera_man.rb
+++ lib/camera_man.rb
@@ -3,6 +3,20 @@
 require_relative 'camera_man/version'
 
 module CameraMan
-  class Error < StandardError; end
-  # Your code goes here...
+  require 'camera_man/config'
+  module Errors
+    require 'camera_man/errors'
+  end
+
+  module Medias
+    require 'camera_man/medias/video'
+  end
+
+  require 'camera_man/project'
+
+  private
+  
+  module Utils
+    require 'camera_man/utils/file_path'
+  end
 end
